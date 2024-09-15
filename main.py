0db388dcd53ac98a0d9ebe9f750f206d68f2b410
Yash Sakhuja <yash.sakhuja01@gmail.com>
2024-09-15 16:35:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from PyPDF2 import PdfReader
+from pypdf import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
